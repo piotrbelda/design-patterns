dfadf0cae1f1094ca14312157fdd0a24147066b5
Piotr Bełda <piotrek.belda@wp.pl>
2024-09-14 14:40:56 +0200

    singleton

---
+++ creational/singleton/singleton.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs) -> Any:
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Singleton(metaclass=SingletonMeta):
+    def some_logic(self):
+        pass
+
+
+if __name__ == '__main__':
+    s1 = Singleton()
+    s2 = Singleton()
+
+    assert s1 != s2
