dca9eaeeec6b4d6a3b35de655a062f4b697470f8
Piotr Bełda <piotrek.belda@wp.pl>
2024-09-14 15:11:05 +0200

    singleton

--- creational/singleton/singleton.py
+++ creational/singleton/singleton.py
@@ -12,12 +12,18 @@ class SingletonMeta(type):
 
 
 class Singleton(metaclass=SingletonMeta):
+    def __init__(self, x: int, y: str) -> None:
+        self.x = x
+        self.y = y
+
     def some_logic(self):
         pass
 
 
 if __name__ == '__main__':
-    s1 = Singleton()
-    s2 = Singleton()
+    s1 = Singleton(1, '2')
+    s2 = Singleton(2, '3')
 
     assert s1 is s2
+    assert s1.x == s2.x
+    assert s1.y == s2.y
