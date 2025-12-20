71c3bf6a2fcc406a307fd4e69a7d0df01766665f
Piotr Bełda <piotrek.belda@wp.pl>
2025-12-20 14:37:31 +0100

    implement as generic decorator

--- behavioral/retry/main.py
+++ behavioral/retry/main.py
@@ -1,13 +1,36 @@
 import random
 import time
-from typing import Callable
+from functools import wraps
+from typing import Any, Callable
 
 import httpx
 
 API_URL = "https://api.chucknorris.io/jokes/random"
 
 
+def retry(retries: int = 3, delay: float = 1.0, backoff: float = 2.0):
+    def decorator[T](func: Callable[..., T]) -> Callable[..., T]:
+        @wraps
+        def wrapper(*args: Any, **kwargs: Any) -> T:
+            for attempt in range(1, retries + 1):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    if attempt == retries:
+                        raise
+                
+                sleep_time = delay * (backoff ** attempt)
+                print(f"Attempt {attempt} failed :(, retrying in {sleep_time} seconds...")
+                time.sleep(sleep_time)
+
+        return wrapper
+
+    return decorator
+
+
+@retry()
 def fetch_chuck_joke(threshold: float = 0.8) -> str:
+    """Get random joke from Chuck Norris API"""
     if (random_value := random.random()) < threshold:
         raise ValueError(f"Value exceeded {random_value:0.3f}!")
 
@@ -17,19 +40,5 @@ def fetch_chuck_joke(threshold: float = 0.8) -> str:
     return data["value"]
 
 
-def retry[T](func: Callable[[], T], retries: int = 3, backoff: int = 1) -> T:
-    try:
-        return func()
-
-    except Exception as e:
-        if retries == 0:
-            print("Cannot fetch result :(")
-            raise
-
-        print(f"Exception has occured: {str(e)}, trying again...")
-        time.sleep(backoff)
-        return retry(func, retries - 1, backoff)
-
-
-joke = retry(fetch_chuck_joke)
+joke = fetch_chuck_joke()
 print(f"The joke is: {joke}")
