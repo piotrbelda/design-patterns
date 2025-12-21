007b47ef92ee7ee063dba1f5aff54cdceba590c5
Piotr Bełda <piotrek.belda@wp.pl>
2025-12-21 19:31:23 +0100

    apply context pattern

--- behavioral/context/main.py
+++ behavioral/context/main.py
@@ -1,4 +1,6 @@
 import logging
+from dataclasses import dataclass
+from typing import Any
 
 from sqlalchemy.orm import Session
 
@@ -6,20 +8,43 @@ from db import db_session, init_db
 from model import Article
 
 
-def render_article(
-    user_id: int, article_id: int, db: Session, logger: logging.Logger, api_key: str
-) -> str:
-    if user_id != 42:
-        logger.error(f"Unauthorized access by user: {user_id}")
-        return "<p>Unauthorized</p>"
+@dataclass
+class AppContext:
+    user_id: int
+    db: Session
+    logger: logging.Logger
+    config: dict[str, Any]
 
+
+def retrieve_article(article_id: int, db: Session, logger: logging.Logger) -> Article | None:
     article = db.query(Article).filter(Article.id == article_id).first()
     if not article:
         logger.error(f"Article {article_id} not found :()")
+        return None
+
+    return article
+
+def render_article(article: Article, logger: logging.Logger) -> str:
+    logger.info(f"Rendering article {article.id}")
+    return f"<h1>{article.title}</h1><p>{article.body}</p>"
+
+
+def send_to_external_service(html: str, api_key: str) -> None:
+    print(f"sending to API with key: {api_key[:4]}... content: {html[:30]}")
+
+
+def publish_article(article_id: int, context: AppContext) -> str | None:
+    if context.user_id != 42:
+        context.logger.error(f"Unauthorized access by user {context.user_id}")
+        return "<p>Unauthorized</p>"
+
+    article = retrieve_article(article_id, context.db, context.logger)
+    if not article:
         return "<p>Article not found.</p>"
 
-    logger.info(f"Rendering article {article_id} using API key: {api_key[:4]}...")
-    return f"<h1>{article.title}</h1><p>{article.body}</p>"
+    html = render_article(article, context.logger)
+
+    send_to_external_service(html, context.config["api_key"])
 
 
 def main() -> None:
@@ -29,13 +54,16 @@ def main() -> None:
     init_db()
 
     with db_session() as session:
-        api_key = "test1234"
+        context = AppContext(
+            user_id=42,
+            db=session,
+            logger=logger,
+            config={
+                "api_key": "test1234",
+            },
+        )
 
-        html = render_article(42, 1, session, logger, api_key)
-        print(html)
-
-        html = render_article(41, 999, session, logger, api_key)
-        print(html)
+        publish_article(1, context)
 
 
 if __name__ == "__main__":
